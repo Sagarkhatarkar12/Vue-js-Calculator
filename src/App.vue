4639de90ee2c7af3bd5fa06b359bc6cd9ba5d506
SDB <91771020+Sagarkhatarkar12@users.noreply.github.com>
2025-05-18 10:11:10 +0530

    basic design complete

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+    <div class="center">
+
+        <Calculator/>
+    </div>
+    
+</template>
+
+<style scoped>
+.center{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,96 @@
+<script scoped>
+
+</script>
+
+<template>
+<div class="calculator">
+    <div class="calculator-display">
+  <input type="text" value="3939" class="calculator-display-input" readonly>
+
+    </div>
+    <div class="calculator-button">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>/</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>X</button>
+
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+
+      <button>.</button>
+      <button>0</button>
+      <button>0</button>
+      <button>=</button>
+
+
+    </div>
+
+</div>
+</template>
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+.calculator{
+  height: 60vh;
+  width: 20vw;
+  background-color:#333132;
+  overflow: hidden;
+  border-radius: 1rem;
+}
+.calculator-display{
+  width: 100%;
+  height: 30%;
+  display: flex;
+  align-items: flex-end;
+}
+.calculator-display-input{
+  border: none;
+  height: 80%;
+  width: 100%;
+  text-align: right;
+  color: white;
+  font-size: 3rem;
+background-color: transparent;
+  outline: none;
+}
+.calculator-button{
+  display: grid;
+  grid-template-columns:repeat(4,1fr);
+
+ 
+  gap: 0.5rem;
+  height: 70%;
+  padding: 1rem;
+  /* grid-template-rows: 2rem; */
+  
+
+  /* grid-template-rows: repeat(4,2fr); */
+
+}
+.calculator-button button{
+  height: 100%;
+  width: 100%;
+  color:white;
+  background-color: #1b464d;
+  border: none;
+  outline: none;
+  font-size: 4em;
+   border-radius: 1rem;
+}
+.calculator-button button:hover{
+  background-color: #f7941d;
+  
+
+  cursor: pointer;
+}
+</style>
