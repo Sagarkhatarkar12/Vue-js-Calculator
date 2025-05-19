d2a2a57be8bcf5d30845811a2cd66377e416e0d1
SDB <91771020+Sagarkhatarkar12@users.noreply.github.com>
2025-05-19 10:29:10 +0530

    basic struture of our calculator and  basic functionality of our calculator

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import Calculator from './components/Calculator.vue'
+import Calculator from './components/Calculator.vue';
+import { ref } from 'vue';
+// in this count value static in this if give that not chane 
+// const count = 0;
+// if we want to change the value of count we need to use ref
+const count = ref(0);
 </script>
 
 <template>
     <div class="center">
-
-        <Calculator/>
+ <Calculator/>
     </div>
     
 </template>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,96 +1,219 @@
-<script scoped>
+<script setup>
+import { ref } from 'vue';
 
+const count = ref(0);
+
+
+let displayValue=ref('0');
+let result;
+function HandleInput(value){
+  switch(value){
+    case "1":{
+      if(displayValue.value==='0'){
+        displayValue.value='1';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "2":{
+      if(displayValue.value==='0'){
+        displayValue.value='2';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "3":{
+      if(displayValue.value==='0'){
+        displayValue.value='3';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "4":{
+      if(displayValue.value==='0'){
+        displayValue.value='4';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "5":{
+      if(displayValue.value==='0'){
+        displayValue.value='5';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "6":{
+      if(displayValue.value==='0'){
+        displayValue.value='6';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "7":{
+      if(displayValue.value==='0'){
+        displayValue.value='7';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "8":{
+      if(displayValue.value==='0'){
+        displayValue.value='8';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "9":{
+      if(displayValue.value==='0'){
+        displayValue.value='9';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "0":{
+      if(displayValue.value==='0'){
+        displayValue.value='0';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "+":{
+      if(displayValue.value==='0'){
+        displayValue.value='+';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "*":{
+        if(displayValue.value==='0'){
+        displayValue.value='+';
+      }else{
+        displayValue.value+=value;
+      }
+      break;
+    }
+    case "/":{
+      if(displayValue.value === "0"){
+        displayValue.value = '/';
+      }
+      else{
+        displayValue.value += value;
+
+      }
+      break;
+    }
+    case "=":{
+      
+      result = displayValue.value;
+      result = eval(result);
+      console.log(result)
+      displayValue.value = result;
+    }
+    case "back":{
+displayValue.value =  displayValue.value.slice(0,-1);
+
+break;
+    }
+    
+  }
+}
 </script>
 
 <template>
-<div class="calculator">
+  <div class="calculator">
     <div class="calculator-display">
-  <input type="text" value="3939" class="calculator-display-input" readonly>
-
+      <input
+        type="text"
+     :value=displayValue
+        class="calculator-display-input"
+        readonly
+      />
     </div>
+
     <div class="calculator-button">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>/</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>X</button>
-
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>+</button>
-
-      <button>.</button>
-      <button>0</button>
-      <button>0</button>
-      <button>=</button>
-
-
+      <button @click="HandleInput('1')" >1</button>
+       <!-- <button>{{ count}}</button> -->
+      <button @click="HandleInput('2')" >2</button>
+      <button @click="HandleInput('3')">3</button>
+      <button @click ="HandleInput('/')">/</button>
+      <button @click="HandleInput('4')">4</button>
+      <button @click="HandleInput('5')">5</button>
+      <button @click="HandleInput('6')">6</button>
+      <button @click="HandleInput('*')">X</button>
+      <button @click="HandleInput('7')">7</button>
+      <button @click="HandleInput('8')">8</button>
+      <button @click="HandleInput('9')">9</button>
+      <button @click="HandleInput('+')">+</button>
+      <button @click="HandleInput('.')">.</button>
+      <button @click="HandleInput('0')">0</button>
+      <button @click="HandleInput('back')"><i class="ri-arrow-left-line"></i></button>
+      <button @click="HandleInput('=')">=</button>
     </div>
-
-</div>
+  </div>
 </template>
+
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-size: 62.5%;
 }
-.calculator{
-  height: 60vh;
+.calculator {
+  height: 65vh;
   width: 20vw;
-  background-color:#333132;
+  background-color: #333132;
   overflow: hidden;
   border-radius: 1rem;
 }
-.calculator-display{
+.calculator-display {
   width: 100%;
   height: 30%;
   display: flex;
   align-items: flex-end;
 }
-.calculator-display-input{
+.calculator-display-input {
   border: none;
   height: 80%;
   width: 100%;
   text-align: right;
   color: white;
   font-size: 3rem;
-background-color: transparent;
+  padding: 1rem 2rem;;
+  background-color: transparent;
   outline: none;
 }
-.calculator-button{
+.calculator-button {
   display: grid;
-  grid-template-columns:repeat(4,1fr);
-
- 
+  grid-template-columns: repeat(4, 1fr);
   gap: 0.5rem;
   height: 70%;
   padding: 1rem;
-  /* grid-template-rows: 2rem; */
-  
-
-  /* grid-template-rows: repeat(4,2fr); */
-
 }
-.calculator-button button{
+.calculator-button button {
   height: 100%;
   width: 100%;
-  color:white;
+  color: white;
   background-color: #1b464d;
   border: none;
   outline: none;
   font-size: 4em;
-   border-radius: 1rem;
+  border-radius: 1rem;
 }
-.calculator-button button:hover{
+.calculator-button button:hover {
   background-color: #f7941d;
-  
-
   cursor: pointer;
 }
 </style>
